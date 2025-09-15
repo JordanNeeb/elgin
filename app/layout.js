6a497143a4bf8005febfb2df5659b5f5a721099c
JordanNeeb <jordanscottneeb@gmail.com>
2025-09-14 19:06:08 -0500

    adds tailwind login form layout

--- app/layout.js
+++ app/layout.js
@@ -18,9 +18,9 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className="h-full bg-white dark:bg-gray-900">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`h-full ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
